869364e2b9ebd936c90a5374d2cc0c146acba9f6
zhao yang <zhaoyangchina@hotmail.com>
2013-09-27 20:09:52 +0800

    next/next* done, basic affect - zy

--- source/CFG.h
+++ source/CFG.h
@@ -1,6 +1,7 @@
 #ifndef CFG_H
 #define CFG_H
 #include "CFGNode.h"
+#include <algorithm>
 class CFG
 {
 public:
@@ -10,9 +11,21 @@ public:
 
 	vector<int> getNext(int stmtNo);
 	vector<int> getPrev(int stmtNo);
-
+	vector<int> getNextStar(int stmtNo);
+	vector<int> getPrevStar(int stmtNo);
+	bool isNext(int stmtNo1, int stmtNo2);
+	bool isNextStar(int stmtNo1,int stmtNo2);
 	void buildCFGParentList(int stmtNo);
+
+	
+
 	vector<int> visited;
+
+private:
+	vector<int> resultList;
+	void getNextStarRecursive(int stmtNo);
+	void getPrevStarRecursive(int stmtNo);
+	bool contains(vector<int> list, int stmtNo);
 };
 
 #endif
