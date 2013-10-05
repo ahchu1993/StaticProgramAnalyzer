7f08c41b5cab608efa436b845435f74fe3820bb3
Duphy <dyzhwx@gmail.com>
2013-10-05 19:13:52 +0800

    asd

--- source/QueryEvaluator.h
+++ source/QueryEvaluator.h
@@ -62,7 +62,7 @@ public:
             2 unselected entity: 50
             1 selected entity: 100
             2 selected eneity: 200
-      *//Users/Andy/Workplace/cs3202/source/Results.cpp
+      */
     /*
       1. sort the desAbstr and eliminate duplicate desAbstr
       2. Querying all the desAbstr with only constants
