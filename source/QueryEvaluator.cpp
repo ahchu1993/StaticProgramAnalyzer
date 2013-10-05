a7776d6c5f91cf4196d9a70e0581010a2fba96e7
victor <victorpham93@gmail.com>
2013-10-05 15:31:29 +0800

    Add process query

--- source/QueryEvaluator.cpp
+++ source/QueryEvaluator.cpp
@@ -1,5 +1,5 @@
 //
-//  QueryEvaluator.cpp
+//  QueryEvaluator.cpp9
 //  cs3202
 //
 //  Created by Zhao Weixiang on 16/9/13.
@@ -16,20 +16,33 @@ list<string> QueryEvaluator::processQuery(string query){
     else{
         //store all the parsed query infomation
         QueryEvaluator::Qprocessor.group_relations();//group the relations
-        entity = Qprocessor.getSymbols();//declaration type, name
-		result = Qprocessor.getResults();//select clause
-		desAbstr = Qprocessor.getRelations();//all the relations
-		pattern = Qprocessor.getPatterns();//all the patterns
-        attr_pairs = Qprocessor.getAttrPairs();//with
+		entity = Qprocessor.declaration_reffs;//declaration type, name
+		result = Qprocessor.result_reffs;//select clause
+		constant_relations = (Qprocessor.constant_relations);//all the relations
+		//grouped_relations = & Qprocessor.grouped_relations;
+       
         //start to evaluate query
         initialzeValueTable(entity);
-        processAttrParis(attr_pairs);
-        processPattern(pattern);
-        processRelations(desAbstr);
+        processConstantRelations();
+        
+        processGroupedRelations();
     }
     return results;
 }
+bool processConstantRelations(){
+	/*go thru constant_relations
+		baseRelation* b = constant_relations(i);
+		if(b->type=="des")
+		{
+			designAbstraction* da = static_cast<designAbstraction*>(b);
+			da->relation_type;
+			pkb ->
+		}else (b->type)
+		*/
+}
 
+bool processGroupedRelations(){
+}
 //store all the possible values for each synonmy
 void QueryEvaluator::initialzeValueTable(vector<QueryPreprocessor::entityReff> entities){
     for (int i =0; i<entity.size(); i++) {
@@ -47,35 +60,42 @@ void processAttrPairs(vector<QueryPreprocessor::attr_compare> attr_pairs){
 
 
 }
-void processPattern(vector<QueryPreprocessor::pattern> pattern){
+void processPattern(vector<pattern> pattern){
 
 
 }
-void processRelations(vector<QueryPreprocessor::designAbstraction> desAbstr){
+void QueryEvaluator::processRelations(vector<designAbstraction> desAbstr){
     for (int i=0; i<desAbstr.size(); i++) {
-        QueryPreprocessor::designAbstraction relation = desAbstr.at(i);
+        designAbstraction relation = desAbstr.at(i);
         if (relation.relation_type.compare("Modifies")==0) {
-            
+		
         }
         else if (relation.relation_type.compare("Calls")==0){
+			pkb->getCall(relation.ref1,relation.ref1_type, relation.ref2, relation.ref2_type);
         }
         else if (relation.relation_type.compare("Calls*")==0){
+
         }
         else if (relation.relation_type.compare("Uses")==0){
+			pkb->getUse(relation.ref1,relation.ref1_type, relation.ref2, relation.ref2_type);
         }
         else if (relation.relation_type.compare("Parent")==0){
+			pkb->getParent(relation.ref1,relation.ref1_type, relation.ref2, relation.ref2_type);
         }
         else if (relation.relation_type.compare("Parent*")==0){
         }
         else if (relation.relation_type.compare("Follows")==0){
+			pkb->getFollow(relation.ref1,relation.ref1_type, relation.ref2, relation.ref2_type);
         }
         else if (relation.relation_type.compare("Follows*")==0){
         }
         else if (relation.relation_type.compare("Next")==0){
+			pkb->getNext(relation.ref1,relation.ref1_type, relation.ref2, relation.ref2_type);
         }
         else if (relation.relation_type.compare("Next*")==0){
         }
         else if (relation.relation_type.compare("Affects")==0){
+			
         }
         else if (relation.relation_type.compare("Affects*")==0){
         }
